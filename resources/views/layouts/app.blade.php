d3a243cffb99924e4c500d9ac5f0377bbaba2c4a
Esi're <esiri92@gmail.com>
2020-07-09 22:00:24 +0100

    Added app layout. Completed nav bar.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Movie App</title>
+  <link rel="stylesheet" href="{{ mix('css/tailwind.css') }}">
+</head>
+<body class="font-sans bg-gray-900 text-white">
+<nav class="border-b border-gray-800">
+  <div class="container mx-auto flex justify-between px-4 py-6">
+    <ul class="flex items-center">
+      <li><a href="#">MovieApp</a></li>
+      <li class="ml-16"><a href="#" class="hover:text-gray-300">Movies</a></li>
+      <li class="ml-6"><a href="#" class="hover:text-gray-300">TV Shows</a></li>
+      <li class="ml-6"><a href="#" class="hover:text-gray-300">Actors</a></li>
+    </ul>
+
+    <div class="flex items-center">
+      <div class="relative">
+        <input type="text"
+               class="text-sm bg-gray-800 rounded-full w-64 px-4 py-1 pl-8 focus:shadow-outline focus:outline-none"
+               placeholder="Search">
+      </div>
+      <div class="ml-4">
+        <a href="#">
+          <img src="{{ asset('images/avatar.jpg') }}" alt="avatar" class="rounded-full w-8 h-8">
+        </a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+@yield('content')
+</body>
+</html>
