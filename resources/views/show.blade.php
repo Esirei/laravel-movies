a6114da0aac12d1fbf7524bd62a0090e2576ea77
Esi're <esiri92@gmail.com>
2020-07-09 23:53:29 +0100

    Added cast and images section.

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -52,4 +52,40 @@
     </div>
   </section>
   <!-- end of movie-info -->
+
+  <section class="movie-cast border-b border-gray-800">
+    <div class="container mx-auto px-4 py-16">
+      <h2 class="text-4xl font-semibold">Cast</h2>
+
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+        <div class="mt-8">
+          <a href="#">
+            <img src="{{ asset('images/actor1.jpg') }}" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-300">
+          </a>
+          <div class="mt-2">
+            <a href="#" class="text-lg mt-2 hover:text-gray-300">Real Name</a>
+            <p class="text-gray-400 text-sm mt-1">
+              John Smith
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end of movie-cast -->
+
+  <section class="movie-images border-b border-gray-800">
+    <div class="container mx-auto px-4 py-16">
+      <h2 class="text-4xl font-semibold">Images</h2>
+
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <div class="mt-8">
+          <a href="#">
+            <img src="{{ asset('images/image1.jpg') }}" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-300">
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end of movie-images -->
 @endsection
