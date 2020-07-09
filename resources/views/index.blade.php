b31e38c43140d57ce5b84218023e5361b57f29d4
Esi're <esiri92@gmail.com>
2020-07-09 22:45:28 +0100

    Added now playing section.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="container mx-auto px-4 pt-16">
-    <div class="popular-movies">
+    <section class="popular-movies">
       <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
         Popular Movies
       </h2>
@@ -28,6 +28,34 @@
           </div>
         </div>
       </div>
-    </div>
+    </section>
+
+    <section class="now-playing-movies py-24">
+      <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
+        Now Playing
+      </h2>
+
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+        <div class="mt-8">
+          <a href="#">
+            <img src="{{ asset('images/parasite.jpg') }}" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-300">
+          </a>
+          <div class="mt-2">
+            <a href="#" class="text-lg mt-2 hover:text-gray-300">Parasite</a>
+            <div class="flex items-center text-gray-400 text-sm mt-1">
+              <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24">
+                <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"/>
+              </svg>
+              <span class="ml-1">85%</span>
+              <span class="mx-2">|</span>
+              <span>Jul 09, 2020</span>
+            </div>
+            <div class="text-gray-400 text-sm">
+              Action, Thriller, Comedy
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 @endsection
