cfed2310dc6b3dc6ecb55a3da047c61fae08b1da
Esi're <esiri92@gmail.com>
2020-07-09 22:53:49 +0100

    Updated nav bar to be responsive.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,21 +10,21 @@
 </head>
 <body class="font-sans bg-gray-900 text-white">
 <nav class="border-b border-gray-800">
-  <div class="container mx-auto flex justify-between px-4 py-6">
-    <ul class="flex items-center">
+  <div class="container mx-auto flex flex-col md:flex-row justify-between px-4 py-6">
+    <ul class="flex flex-col md:flex-row items-center">
       <li><a href="#">MovieApp</a></li>
-      <li class="ml-16"><a href="#" class="hover:text-gray-300">Movies</a></li>
-      <li class="ml-6"><a href="#" class="hover:text-gray-300">TV Shows</a></li>
-      <li class="ml-6"><a href="#" class="hover:text-gray-300">Actors</a></li>
+      <li class="md:ml-16 mt-3 md:mt-0"><a href="#" class="hover:text-gray-300">Movies</a></li>
+      <li class="md:ml-6 mt-3 md:mt-0"><a href="#" class="hover:text-gray-300">TV Shows</a></li>
+      <li class="md:ml-6 mt-3 md:mt-0"><a href="#" class="hover:text-gray-300">Actors</a></li>
     </ul>
 
-    <div class="flex items-center">
-      <div class="relative">
+    <div class="flex flex-col md:flex-row items-center">
+      <div class="relative mt-3 md:mt-0">
         <input type="text"
                class="text-sm bg-gray-800 rounded-full w-64 px-4 py-1 pl-8 focus:shadow-outline focus:outline-none"
                placeholder="Search">
       </div>
-      <div class="ml-4">
+      <div class="md:ml-4 mt-3 md:mt-0">
         <a href="#">
           <img src="{{ asset('images/avatar.jpg') }}" alt="avatar" class="rounded-full w-8 h-8">
         </a>
