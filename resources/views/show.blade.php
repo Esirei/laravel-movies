badb6999947233cca1b9118a058460861042bc4d
Esi're <esiri92@gmail.com>
2020-07-09 23:13:54 +0100

    Updated show.blade.php with custom width.

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="movie-info border-b border-gray-800">
     <div class="container mx-auto px-4 py-16 flex">
-      <img src="{{ asset('images/parasite.jpg') }}" alt="parasite" style="width: 24rem">
+      <img src="{{ asset('images/parasite.jpg') }}" alt="parasite" class="w-96">
       <div class="ml-24">
         Details here...
       </div>
