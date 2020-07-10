134ea725b09e969dbc9bd10ef2fad504bd133d5a
Esi're <esiri92@gmail.com>
2020-07-10 14:07:23 +0100

    Updated search-dropdown.blade.php with search item.

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -2,4 +2,14 @@
   <input type="text"
          class="text-sm bg-gray-800 rounded-full w-64 px-4 py-1 pl-8 focus:shadow-outline focus:outline-none"
          placeholder="Search">
+
+  <div class="absolute bg-gray-800 rounded w-64 mt-4 text-sm">
+    <ul>
+      <li class="border-b border-gray-700">
+        <a href="#" class="block hover:bg-gray-700 px-3 py-3">
+          Avengers: End Game
+        </a>
+      </li>
+    </ul>
+  </div>
 </div>
