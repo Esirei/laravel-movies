fc16bde76f954c23490c45d89562a9ce3cdca3c1
Esi're <esiri92@gmail.com>
2020-07-10 23:05:00 +0100

    Close dropdown on clicking out of dropdown.

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,4 +1,4 @@
-<div class="relative mt-3 md:mt-0">
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
   <input type="text" placeholder="Search" wire:model.debounce.500ms="search"
          class="text-sm bg-gray-800 rounded-full w-64 px-4 py-1 pl-8 focus:shadow-outline focus:outline-none">
 
@@ -7,7 +7,7 @@
   </div>
 
   @if(strlen($search) > 2)
-    <div class="absolute bg-gray-800 rounded w-64 mt-4 text-sm">
+    <div class="absolute bg-gray-800 rounded w-64 mt-4 text-sm" x-show="isOpen">
       @if(count($this->results) > 0)
         <ul>
           @foreach($this->results as $result)
