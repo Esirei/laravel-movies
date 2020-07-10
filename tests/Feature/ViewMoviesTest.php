06a02816dfa1f81d767cc16048e61e30e677fd61
Esi're <esiri92@gmail.com>
2020-07-10 03:41:42 +0100

    Added index page test.

---
+++ tests/Feature/ViewMoviesTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class ViewMoviesTest extends TestCase
+{
+    public function testTheIndexPageShows()
+    {
+        $popularMovies = $this->getTestJson('popular-movies.json');
+        $nowPlaying = $this->getTestJson('now-playing.json');
+        $genres = $this->getTestJson('genres.json');
+
+        Http::fake([
+            'https://api.themoviedb.org/3/movie/popular' => Http::response($popularMovies, 200),
+            'https://api.themoviedb.org/3/movie/now_playing' => Http::response($nowPlaying, 200),
+            'https://api.themoviedb.org/3/genre/movie/list' => Http::response($genres, 200),
+        ]);
+
+        $response = $this->get(route('movies.index'));
+
+        $response->assertSuccessful();
+        $response->assertSee('Popular Movies');
+        $response->assertSeeText('Master and Commander: The Far Side of the World');
+        $response->assertSeeText('Now Playing');
+        $response->assertSeeText('The Matrix');
+    }
+
+    private function getTestJson($file)
+    {
+        return json_decode($this->getTestFile($file), true);
+    }
+
+    private function getTestFile($file)
+    {
+
+        return file_get_contents(storage_path("test_data/$file"));
+    }
+}
